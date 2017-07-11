b61978d377ea08da6d56bf4f936d63d5bb995792
Arik Liber <leonliber@gmail.com>
2017-07-11 15:25:43 +0300

    Major refactor, take out scraper to a new module, clean up __init__

---
+++ article_scraper.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import config
+import logging
+import requests
+from google.cloud import datastore
+from google.cloud import error_reporting
+import google.cloud.logging
+from newspaper import Article
+
+def scrape(url, language='en'):
+  ''' Scrape given url using supplied or default language
+
+  Args:
+    url (str): Article's URL for parsing
+    language (str, optional): Language to parse in. Defaults to 'en'
+
+  Returns:
+    Parsed text if succeeds, empty string otherwise
+  '''
+  page = Article(url = url, language = language)
+  attempts = 0
+  success = False
+  # Exception raised following this bug: https://github.com/codelucas/newspaper/issues/357
+  while(attempts < 4):
+      try:
+          attempts += 1
+          page.download()
+          page.parse()
+          success = True
+          break
+      except BaseException as e:
+          print('Error in parsing {}, attempt #{}'.format(url, attempts) , str(e))
+          pass
+
+  return page.text if success else ''
+
+#Update article entity by adding scraped content
+def updateArticleEnt(keyPub, keyNum, content):
+  #Retrieve article from Google Cloud Datastore
+  ds = datastore.Client(project= str(config.PROJECT_ID))
+  article_key= datastore.Key("publishers", keyPub,"articles", str(keyNum), project=str(config.PROJECT_ID))
+  article_ent = ds.get(key=article_key)
+
+  #Update article's content and status
+  article_ent["content"]= content
+  article_ent["status"]= "scraped"
+
+  #Return updated article to datastore
+  ds.put(article_ent)
+
+
+#Sends post request to snippet-matching function
+def initiatePost(content, keyPub, keyNum, lang):
+  #TODO: Add a smart_function_url
+  requests.post('smart_function_url', data = {"content": content,"publisher": keyPub, "articleNumber": keyNum, "lang": lang})
+
+
+'''Scrapes the article, updates the Article entity in datastore, and then
+initiates smart snippet-matching function'''
+def process(keyPub, keyNum, url, lang):
+  content = scrape(url, lang)
+  updateArticleEnt(keyPub,keyNum, content)
+  #initiatePost(content, keyPub, keyNum, lang)
+  return content
+
+# Exectue if run independantly
+if __name__ == '__main__':
+  print(process('gadgety.co.il', 183738, 'http://www.gadgety.co.il/183738/spiderman-homecoming-review/', 'he'))
--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 #!/usr/bin/env python
 
-import config
-from __init__ import create_app
+from flask import current_app, Flask, redirect, url_for, request
+import requests
+import json
 
-#Create flask app
-app = create_app(config)
+import config
+import article_scraper
+import snippets_keywords_builder
+
+
+#TODO: adjust Flask input arg to reflect package or move all to one file
+app = Flask(__name__)
+app.config.from_object(config)
+
+# Configure logging
+#ADD THIS BACK FOR DEPLOYMENT#client = google.cloud.logging.Client(app.config['PROJECT_ID'])
+# Attaches a Google Stackdriver logging handler to the root logger
+#ADD THIS BACK FOR DEPLOYMENT#client.setup_logging(logging.INFO)
+
+# Receive POST from ad server and begin scrape/snippet matching process
+@app.route('/scrapeArticle', methods=['POST'])
+def scrapeArticle():
+  data = request.get_json()
+
+  keyPub = data.get('publisher')
+  keyNum = data.get('articleNumber')
+  url = data.get('url')
+  lang = data.get('language')
+
+  if not data or not keyPub or not keyNum or not url or not lang:
+      return make_response("Missing Data", 500)
+
+  article = article_scraper.process(keyPub, keyNum, url, lang)
+  return json.dumps({'article': article})
+
+@app.route('/buildSnippetKeywords', methods=['POST'])
+def buildSnippetKeywords():
+  data = request.get_json()
+  snippetId = data.get('snippetId')
+  if not data or not snippetId:
+      return make_response('No snippet id supplied', 404)
+
+  res = snippets_keywords_builder.setSnippetWeightedKeywords(snippetId)
+  return json.dumps({'summary': res})
 
 
 # This is only used when running locally. When running live, gunicorn runs
 # the application.
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+  app.run(host='127.0.0.1', port=8080, debug=True)

10912bf0dca60debba2809505c35d2e08cc6af5b
Logan Horowitz <lhh48@cornell.edu>
2017-07-13 16:14:01 +0000

    Added Twitter API access values

--- config.py
+++ config.py
@@ -3,11 +3,6 @@
 # The secret key is used by Flask to encrypt session cookies.
 SECRET_KEY = 'secrets'
 
-# There are three different ways to store the data in the application.
-# You can choose 'datastore', 'cloudsql', or 'mongodb'. Be sure to
-# configure the respective settings for the one you choose below.
-# You do not have to configure the other data backends. If unsure, choose
-# 'datastore' as it does not require any additional configuration.
 DATA_BACKEND = 'datastore'
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
@@ -16,3 +11,9 @@ PROJECT_ID = 'simoti-171512'
 
 SEARCHENGINE_KEY = 'AIzaSyAx_o3OOlY92fsPUfQaUBzUlXG8-a-Zsm0'
 SEARCHENGINE_ID = '000161978559585141097:55s91q7tm4q'
+
+# Twitter API Access Values
+consumer_key = '5We5CMiZamhDR0AzAeVlSq8jk'
+consumer_secret = 'mnYfyCqJC0hEqw9Xw2DZJPz9r97DYBWbA3hK98EY69c2YfGcsV'
+access_token = '12833912-fpmakSwu52RPjPry7VV5pzOCIlRq0RegjQ4zvkkom'
+access_token_secret = 'EY5G6dmEXvGKoubm02MOYnZRPtnpSP5lf7M1q8xXTPEAf'
